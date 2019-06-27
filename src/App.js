6e6eef2a6a96e5ef6a4eb1e230166d2af88f13af
shefali1996 <shefali@xcellencetechnologies.info>
2019-06-27 13:50:24 +0530

    Todo Added again

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../src/App.css';
+import TextBox from './component/TextBox';
+import ShowTodo from './component/ShowTodo';
 
 class App extends Component{
-state={
 
-}
+  state={
+    arr:[],
+    text:""
+  }
+
+  getValue=(e)=>{
+    this.setState({
+      text:e.target.value,
+    });
+  }
+
+  submit=()=>{
+    if(this.state.text!=""){
+      this.setState({
+        arr:this.state.arr.concat(this.state.text),
+        text:""
+      })
+    }
+  }
+
+  enterKey=(e)=>{
+    if(e.key=="Enter" && e.target.value!=""){
+      this.submit()
+    }
+  }
+
   render(){
     return(
-      <div></div>
-    );
-}
-
+      <div>
+        <ShowTodo data={this.state}/>
+        <TextBox data={this.state}
+        getValue={this.getValue}
+        submit={this.submit}
+        enterKey={this.enterKey}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
